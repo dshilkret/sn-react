cef6fd253cc7c7bf80497a61c5d056697cb4093a
Chris Yang <zaidongy@gmail.com>
2019-06-17 10:53:30 -0700

    only add relevant files

---
+++ precache-manifest.651378a7480e5eacd821a4079af49e66.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c010a24f9d6c9bf7083397a0b762f9b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02748a293b5ab791aabe",
+    "url": "/static/css/main.94ad8322.css"
+  },
+  {
+    "revision": "02748a293b5ab791aabe",
+    "url": "/static/js/main.4b66151c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
